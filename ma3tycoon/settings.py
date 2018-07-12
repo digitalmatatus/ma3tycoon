64da99831a5ae84b3a6a454692077557f2d744ac
MundiCollins <mundicollins@gmail.com>
2018-07-12 19:30:37 +0300

    Updated static routes

--- ma3tycoon/settings.py
+++ ma3tycoon/settings.py
@@ -145,10 +145,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 
-STATIC_URL = 'ma3tycoon/static/'
+STATIC_URL = '/ma3tycoon/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'ma3tycoon/static'),
+    os.path.join(BASE_DIR, '/ma3tycoon/static'),
 )
 
 
